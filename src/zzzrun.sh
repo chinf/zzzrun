94e281f6598a3c9c94e68bf37b951aa1390cfbed
Francis Chin <dev@fchin.com>
2017-07-16 23:26:39 +0100

    Added -x option to exclude pools
    Added pool exclusion argument
    Reworked pool parsing

--- src/zzzrun.sh
+++ src/zzzrun.sh
@@ -28,7 +28,7 @@
 VERBOSITY=0
 
 print_log() { # level, message
-  local LEVEL=$1
+  local LEVEL="$1"
   shift 1
   case $LEVEL in
     (err*) echo -e "Error: $*" >&2 ;;
@@ -44,7 +44,9 @@ print_log() { # level, message
 SUBSTOKEN="@zzzrun-pool"
 
 print_usage() {
-  echo "Usage: $0 [options] [-p POOL]... COMMAND [${SUBSTOKEN}] ...
+  echo "Usage:
+  $0 [options] [-p POOL]... [-x POOL]... COMMAND [${SUBSTOKEN}] ...
+
 Run a command only if a ZFS pool has no hard drives in standby or sleep
 power modes.
 
@@ -63,6 +65,9 @@ power modes.
                than one pool.  Any pools specified that are unavailable
                will be ignored.
 
+  -x POOL      Exclude a pool.  Repeat this option to specify more than
+               one pool to exclude.
+
   COMMAND      Command to execute if no hard drives in the pool(s) are in
                standby or sleep modes.
                Optionally, use the token ${SUBSTOKEN} one or more times
@@ -75,17 +80,21 @@ power modes.
 exit 2
 }
 
-ALLPOOLS=yes
-PERPOOL=yes
-POOLARGS=
-while getopts ":svp:" OPT; do
+RUNMODE="per pool"
+while getopts ":svp:x:" OPT; do
   case "${OPT}" in
-    s) PERPOOL=no ;;
+    s) RUNMODE="single" ;;
     v) VERBOSITY=`expr $VERBOSITY + 1` ;;
     p)
-      ALLPOOLS=no
       if [ -n "${OPTARG}" ]; then
         POOLARGS+="${OPTARG} "
+      else
+        print_usage
+      fi
+      ;;
+    x)
+      if [ -n "${OPTARG}" ]; then
+        POOLEXCLUDES+="${OPTARG} "
       else
         print_usage
       fi
@@ -96,45 +105,54 @@ done
 shift $((OPTIND-1))
 if [ -z "$*" ]; then print_usage; fi
 COMMAND="$*"
-print_log debug "COMMAND: $COMMAND"
+print_log debug "Parsed COMMAND: $COMMAND"
 
 #
 # Configuration validation
 #
-ZPOOLLIST=$(env sudo zpool list -H -o name)
-POOLS=
-for POOL in $POOLARGS
+ZPOOLS=$(env sudo zpool list -H -o name)
+if [ -z "${ZPOOLS}" ]; then
+  print_log warn "No pools are available on this system"
+  exit 3
+fi
+if [ -z "${POOLARGS}" ]; then POOLARGS="${ZPOOLS}"; fi
+
+for POOLARG in $POOLARGS
 do
-  # Validate argument against currently available pools
-  for ZPOOL in $ZPOOLLIST
+  for POOLEXCLUDE in $POOLEXCLUDES
   do
-    if [ $POOL = $ZPOOL ]; then POOLS+="${POOL} "; fi
+    if [ "${POOLARG}" = "${POOLEXCLUDE}" ]; then 
+      print_log debug "Excluding pool: ${POOLARG}"
+      continue 2
+    fi
+  done
+
+  for ZPOOL in $ZPOOLS
+  do
+    if [ "${POOLARG}" = "${ZPOOL}" ]; then
+      POOLS+="${POOLARG} "
+      break
+    fi
   done
 done
 
-if [ $ALLPOOLS = "no" ]; then
-  if [ -z "$POOLS" ]; then
-    print_log warn "None of the pools specified are available"
-    print_log debug "ZPOOLLIST: ${ZPOOLLIST}"
-    exit 3
-  fi
-else
-  if [ -n "$ZPOOLLIST" ]; then
-    POOLS="$ZPOOLLIST"
-  else
-    print_log warn "No pools are available on this system"
-    exit 3
-  fi
+if [ -z "${POOLS}" ]; then
+  print_log warn "None of the pools specified are available"
+  print_log debug "POOLARGS: ${POOLARGS}"
+  print_log debug "POOLEXCLUDES: ${POOLEXCLUDES}"
+  print_log debug "ZPOOLS: ${ZPOOLS}"
+  exit 3
 fi
-print_log info "Pool(s) available to process:\n${POOLS}"
-print_log debug "Processing per pool: ${PERPOOL}"
+
+print_log debug "Run mode: ${RUNMODE}"
+print_log info "Pools available to process: ${POOLS}"
 
 #
 # Main functions
 #
 get_pool_disks() { # pool list
   local SCOPE="$1"
-  print_log debug "Finding hard drives in pool(s):\n${SCOPE}"
+  print_log debug "Finding hard drives in pool(s): ${SCOPE}"
 
   # Parse zpool status to get all devices used by the specified pool(s)
   local DEVS=$(env LC_ALL=C sudo zpool status -L $SCOPE \
@@ -151,7 +169,7 @@ get_pool_disks() { # pool list
     | sort -u)
   print_log debug "HDDS:\n${HDDS}"
 
-  echo $HDDS
+  echo "${HDDS}"
 }
 
 count_standby_disks() { # hdds
@@ -162,7 +180,7 @@ count_standby_disks() { # hdds
   print_log debug "DRIVESTATES:\n${DRIVESTATES}"
 
   local UNKNOWNS=$(env echo "${DRIVESTATES}" | grep -cE 'unknown')
-  if [ $UNKNOWNS -gt 0 ]; then
+  if [ "${UNKNOWNS}" -gt 0 ]; then
     print_log warn "Unable to read status for at least one drive"
   fi
 
@@ -172,7 +190,7 @@ count_standby_disks() { # hdds
 run_action() { # pool list
   local SCOPE="$1"
 
-  local CMD=$(env echo $COMMAND | sed "s/${SUBSTOKEN}/${SCOPE}/g")
+  local CMD=$(env echo "${COMMAND}" | sed "s/${SUBSTOKEN}/${SCOPE}/g")
   print_log debug "Executing command '${CMD}':"
 
   $CMD >&2
@@ -183,30 +201,30 @@ run_action() { # pool list
 #
 for POOL in $POOLS
 do
-  if [ $PERPOOL = "yes" ]; then
-    POOLSCOPE="${POOL}"
-  else
+  if [ "${RUNMODE}" = "single" ]; then
     POOLSCOPE="${POOLS}"
+  else
+    POOLSCOPE="${POOL}"
   fi
 
   POOLDISKS=`get_pool_disks "${POOLSCOPE}"`
   if [ -n "${POOLDISKS}" ]; then
     STANDBYCOUNT=`count_standby_disks "${POOLDISKS}"`
     print_log info \
-      "${STANDBYCOUNT} disk(s) standby/sleep in pool(s):\n${POOLSCOPE}"
+      "${STANDBYCOUNT} disk(s) standby/sleep in pool(s): ${POOLSCOPE}"
   else
     STANDBYCOUNT=0
-    print_log info "No hard drives in pool(s):\n${POOLSCOPE}"
+    print_log info "No hard drives in pool(s): ${POOLSCOPE}"
   fi
 
-  if [ $STANDBYCOUNT -eq 0 ]; then
+  if [ "${STANDBYCOUNT}" -eq 0 ]; then
     print_log info "Running command for pool(s): ${POOLSCOPE}"
     run_action "${POOLSCOPE}"
   else
-    print_log info "Skipping execution"
+    print_log info "Skipping execution for pool(s): ${POOLSCOPE}"
   fi
 
-  if [ $PERPOOL = "no" ]; then break; fi
+  if [ "${RUNMODE}" = "single" ]; then break; fi
 done
 
 print_log debug "$0 finished."
